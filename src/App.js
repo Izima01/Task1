35376c5dfde99674201244f392c1e5c034262549
izima01 <kingsleyizima@gmail.com>
2022-11-05 18:31:35 +0100

    bg: fixed a typo error on app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
